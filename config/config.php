1877ed6299cea103b4d34174b4ac1cba9442bdf5
rromina <romina.celintano.quinteros@gmail.com>
2023-08-01 23:52:57 -0300

    Configuro datos para la bd y elimino comentario en config.php

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
 
-//define('URL', 'http://localhost/mvc/');
 define('URL', 'http://' . $_SERVER['HTTP_HOST'] . '//');
 
 //conexion a la base de datos
 define('HOST', 'localhost');
 define('PORT_DB', '3307');
-define('DB', 'f01');
+define('DB', 'viauy_database');
 define('USER', 'root');
-define('PASSWORD', '');
+define('PASSWORD', 'papi5425');
 define('CHARSET', 'utf8mb4');
+
