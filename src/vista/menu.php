f872bfa397bbb6db0b3792fd5b6a0627b11ae555
rromina <romina.celintano.quinteros@gmail.com>
2023-08-01 23:05:22 -0300

    Agrego el menú del proyecto

--- src/vista/menu.php
+++ src/vista/menu.php
@@ -1,6 +1,54 @@
-<nav>
-  <a href="index.php">Principal</a>
-  <a href="index.php?c=persona&m=listar">Listar Personas</a>
-  <a href="index.php?c=producto&m=listar">Listar Productos</a>
-  <a href="index.php?c=producto&m=nuevo">Nuevo producto</a>
-</nav>
+<header id="fixed-color">
+    <a href="index.html" id="link-icon-main" class="link-icon-main"><img class="icon-main" id="icon-main" src="public/img/logos-png/icon-main.png" alt=""></a>
+    <a href="index.html" class="link-icon-movil"><img class="icon-movil" id="icon-main-movil" src="public/img/logos-png/icon-login.png" alt=""></a> 
+    <nav>
+        <ul>
+            <li>
+              <a href="index.html">
+                <div class="nav-icons">
+                  <i class="fa-solid fa-house"></i>
+                  Inicio
+                </div>
+              </a>
+            </li>
+            <li>
+              <a href="empresa.html">
+                <div class="nav-icons">
+                  <i class="fa-solid fa-bus-simple"></i>
+                  Empresa
+                </div>
+              </a>
+            </li>
+            <li>
+              <a href="contacto.html">
+                <div class="nav-icons">
+                  <i class="fa-solid fa-phone"></i>
+                  Contacto
+                </div>
+              </a>
+            </li>
+            <li>
+              <a href="noticias.html">
+                <div class="nav-icons">
+                  <i class="fa-solid fa-newspaper"></i>
+                  Noticias
+                </div>
+              </a>
+            </li>
+            
+          </ul>
+
+    </nav>
+    <div class="icons-box">
+        <div class="idiomas">
+            <select class="language">
+                <option class="option" value="es">ESP</option>
+                <option class="option" value="en">ENG</option>
+            </select>
+            <i class="fa-solid fa-earth-americas icons-nolinks"></i>
+        </div>
+        
+        <a href="login.html"><i class="fa-solid fa-user"></i></a>
+        <i class="fa-solid fa-bars" id="menu"></i>
+    </div>
+</header>
