048921f8cc819caddb62601aef879d0c3290c651
rromina <romina.celintano.quinteros@gmail.com>
2023-08-01 23:04:25 -0300

    Agrego el footer del proyecto

---
+++ src/vista/footer.php
@@ -0,0 +1,66 @@
+<footer>
+        <div class="viauy-footer">
+            <div class="footer-content1">
+                <img class="img-footer" src="public/img/logos-png/Viauy-5.png" alt="">
+                <p>
+                    ViaUY une las ciudades más importantes de los departamentos 
+                    de Montevideo, Florida, San José y Canelones, mediante una 
+                    red de más de 250 servicios diarios.
+                </p>
+            </div>
+
+
+            <ul class="ul">
+                <li><a href="">Sobre nosotros</a></li>
+                <li><a href="">Noticias</a></li>
+                <li><a href="">Agencias</a></li>
+                <li><a href="">Reclamos</a></li>
+            </ul>
+                
+            
+            
+            <ul class="ul">
+                <li><a href="">Contacto</a></li>
+                <li><a href="">Correo</a></li>
+                <li><a href="">Telefono</a></li>
+            </ul>
+            
+            
+            <ul class="ul">
+                <li><a href="">Instagram</a></li>
+                <li><a href="">Twitter</a></li>
+                <li><a href="">Facebook</a></li>
+            </ul>
+            <div class="footer-content2">
+                
+                <ul>
+                    <li><a href="">Sobre nosotros</a></li>
+                    <li><a href="">Noticias</a></li>
+                    <li><a href="">Agencias</a></li>
+                    <li><a href="">Reclamos</a></li>
+                </ul>
+                    
+                
+                
+                <ul>
+                    <li><a href="">Contacto</a></li>
+                    <li><a href="">Correo</a></li>
+                    <li><a href="">Telefono</a></li>
+                </ul>
+                
+                
+                <ul>
+                    <li><a href="">Instagram</a></li>
+                    <li><a href="">Twitter</a></li>
+                    <li><a href="">Facebook</a></li>
+                </ul>
+                
+            </div>
+        </div>
+        <div class="coderwise">
+            <div class="coderwise-content">
+                <p>©2023 - ViaUY | All rights are reserved</p>
+                <p>Created by <a href="">CoderWise</a></p>  
+            </div>
+        </div>
+    </footer>
