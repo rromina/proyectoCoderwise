f0466eaf903fb6272209f9ac9fc9617356dd82f1
Neon7S <santilopeez7s@gmail.com>
2023-11-16 17:45:54 -0300

    actualizando

--- src/vista/contacto/contacto.php
+++ src/vista/contacto/contacto.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ViaUY</title>
+  <title>ViaUY Contacto</title>
 
   <!-- CSS contacto website -->
   <link rel="stylesheet" href="public/css/style.css">
